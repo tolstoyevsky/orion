acfcb397a716572c1d9983c7867953d7fb1976ef
Evgeny Golyshev <eugulixes@gmail.com>
2018-12-08 01:03:57 +0300

    Client: remove everything related to full-screen mode

--- static/terminal/terminal.js
+++ static/terminal/terminal.js
@@ -22,28 +22,6 @@ export class Terminal {
         const _input = new Input(this.screen.$node);
         const _ws = new WebSocket('ws://' + location.host + '/termsocket');
 
-        /*
-         * When entering full-screen mode, figure out an optimal display
-         * resolution.
-         */
-        this.screen.bind('onenterfullscreen', (() => {
-            const cell = this.display.getCellSize();
-            const indent = this.screen.getIndent()
-            const row = (window.screen.height - indent * 2) / cell.height
-            const col = (window.screen.width - indent * 2) / cell.width;
-
-            this.display.setResolution(Math.floor(row), Math.floor(col));
-        }));
-
-        this.screen.bind('onexitfullscreen', (() => {
-            this.display.setResolution(row, col);
-        }));
-
-        this.display.bind('onsetresolution', e => {
-            _fit_screen_size(e.row, e.col);
-            _ws.send('rsz,' + e.row + 'x' + e.col);
-        });
-
         _input.bind('oninput', function(data) {
             _ws.send(data);
         });
